4525bd384b2753b0ad8d1c35f6ac5016d7ca85dd
Matthew Todd <matthew.todd@gmail.com>
2008-10-07 12:53:30 +0300

    Initial User model.

---
+++ db/migrate/20081007095012_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name, :email
+      t.string :encrypted_password, :remember_me_token, :salt, :limit => 40
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081006124308) do
+ActiveRecord::Schema.define(:version => 20081007095012) do
 
   create_table "cards", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(:version => 20081006124308) do
   end
 
   create_table "orders", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "encrypted_password", :limit => 40
+    t.string   "remember_me_token",  :limit => 40
+    t.string   "salt",               :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
   end

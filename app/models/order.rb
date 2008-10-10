c14cf546585873a9b9209511a53e6caa72af5529
Matthew Todd <matthew.todd@gmail.com>
2008-10-10 16:48:18 +0300

    Order.quantity respects individual item quantities.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :items
 
+  def quantity
+    quantity = 0
+    items.each { |item| quantity += item.quantity }
+    quantity
+  end
+
   def total
     total = Money.new(0)
     items.each { |item| total += item.total }
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -3,12 +3,24 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   should_have_many :items
 
+  context 'quantity' do
+    should 'be zero if there are no items' do
+      assert_equal 0, Order.new.quantity
+    end
+
+    should 'be delegated to items' do
+      order = Order.new
+      order.stubs(:items).returns [stub(:quantity => 3), stub(:quantity => 4)]
+      assert_equal 7, order.quantity
+    end
+  end
+
   context 'total' do
     should 'be zero if there are no items' do
       assert_equal Money.new(0), Order.new.total
     end
 
-    should 'be delegates to items' do
+    should 'be delegated to items' do
       order = Order.new
       order.stubs(:items).returns [stub(:total => Money.new(3)), stub(:total => Money.new(4))]
       assert_equal Money.new(7), order.total

68ef65cf0c3cf2ed4fb33c64cbe2cf5d80e2b2db
Matthew Todd <matthew.todd@gmail.com>
2008-10-17 11:27:13 +0300

    Testing out PayPal link.

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,2 +1,6 @@
 # Configure ActiveMerchant in test mode, unless we're running in production.
 ActiveMerchant::Billing::Base.mode = Rails.env.production? ? :production : :test
+
+# Include the view helper.
+require 'active_merchant/billing/integrations/action_view_helper'
+ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)

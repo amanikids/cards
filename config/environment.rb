7145a08f6a408aee1600965c32cce4cbb68cffbd
Matthew Todd <matthew.todd@gmail.com>
2008-10-24 08:09:42 +0300

    Update to Rails 2.1.2, installing iso-3166-country-select plugin.
    The country_select helper is deprecated in Rails proper, due for removal in 2.2.0.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -13,8 +13,8 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
-  config.gem 'BlueCloth', :lib => 'bluecloth'
   config.gem 'activemerchant', :lib => 'active_merchant'
+  config.gem 'BlueCloth', :lib => 'bluecloth'
   config.gem 'haml'
   config.gem 'money', :source => 'http://dist.leetsoft.com'
 

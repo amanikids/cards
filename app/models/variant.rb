71cca86ae68b25716baf42137dcd745a2da4d00c
Matthew Todd <matthew.todd@gmail.com>
2008-10-26 12:43:06 +0300

    Removed unneeded method.

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -4,10 +4,6 @@ class Variant < ActiveRecord::Base
   composed_of :price, :class_name => 'Money', :mapping => [%w(cents cents), %w(currency currency)]
   validates_presence_of :name, :cents, :currency, :product_id
 
-  def downloadable?
-    !file_format.blank?
-  end
-
   def product_name
     product.name
   end

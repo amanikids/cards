bdcf2ebdb10f3b78e15b4f7e9e60b7f004c38a83
Matthew Todd <matthew.todd@gmail.com>
2008-10-29 16:37:43 +0300

    New bank for .00 on all exchanged money.

---
+++ lib/rounding_bank.rb
@@ -0,0 +1,30 @@
+class RoundingBank
+  RATES = { 'CAD_to_GBP' => 0.5, 'CAD_to_USD' => 1.0, 'GBP_to_USD' => 2.0 }
+  
+  def reduce(money, currency)
+    if money.currency == currency
+      money
+    else
+      exchange(money, currency) 
+    end
+  end
+
+  private
+  
+  def exchange(money, currency)
+    exchanged_cents = money.cents * exchange_rate(money.currency, currency)
+    Money.new(round(exchanged_cents.floor), currency)
+  end
+
+  def exchange_rate(from, to)
+    lookup_exchange_rate(from, to) || 1 / lookup_exchange_rate(to, from) || raise(Money::MoneyError.new("Can't exchage from #{from} to #{to}"))
+  end
+  
+  def lookup_exchange_rate(from, to)
+    RATES["#{from}_to_#{to}"]
+  end
+  
+  def round(cents)
+    cents - (cents % 100)
+  end
+end

562112d2fc6a5096ba6561074e8aa0ca2f823fd8
Matthew Todd <matthew.todd@gmail.com>
2008-10-29 15:13:31 +0300

    Just noticed we don't need this method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,11 +9,4 @@ class Test::Unit::TestCase
 
   self.backtrace_silencers << :rails_vendor
   self.backtrace_filters   << :rails_root
-
-  def stub_current_user
-    returning stub(:id => 42, :email => 'email') do |user|
-      @controller.current_user = user
-      User.stubs(:find).with(user.id).returns(user)
-    end
-  end
 end

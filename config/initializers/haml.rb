df7c371212339debfe6675d8ebada0d00dee30d3
Matthew Todd <matthew.todd@gmail.com>
2008-11-19 11:17:57 +0300

    Explicitly initialize Haml.
    Not doing this was causing rake to blow up even before getting to runing tests. I'm wondering if this was somehow caused by my uninstalling the haml gem yesterday.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,2 +1,5 @@
+# It seems like Haml's init.rb isn't being called when we have it vendored as a gem.
+Haml.init_rails(binding)
+
 # Tell Haml to wrap html attributes with double instead of single quotes.
 Haml::Template.options[:attr_wrapper] = '"'

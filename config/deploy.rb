20fb63946b72930a516a9e1de8e6927a092a2b72
Matthew Todd <matthew.todd@gmail.com>
2009-03-04 14:14:45 +0300

    Move deployment to amanikids server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,36 @@
-set :application, 'cards_amanikids_org'
-set :user,        'matthew'
+set :application, 'cards'
+set :repository,  "git://github.com/amanikids/#{application}.git"
 
-set :database,    "#{user}_#{application}_production"
-set :domain_path, "/users/home/#{user}/domains/cards.amanikids.org"
-set :deploy_to,   "#{domain_path}/var/www"
+set :deploy_to, "/var/www/apps/#{application}"
+set :user, 'deploy'
+set :use_sudo, false
 
 set :scm, :git
-set :local_repository,  "woodward:git/#{application}.git"
-set :repository,  "/users/home/#{user}/git/#{application}.git"
+set :local_repository, '.git'
 set :branch, 'master'
 set :git_shallow_clone, 1
 set :git_enable_submodules, true
 
-set :group_writable, false
-set :use_sudo, false
+server 'amanikids.joyeurs.com', :web, :app, :db, :primary => true, :user => user
 
-server 'woodward.joyent.us', :web, :app, :db, :primary => true, :user => user
+
+def create_symlink(path)
+  run "rm -rf #{latest_release}/#{path}; ln -s #{shared_path}/#{path} #{latest_release}/#{path}"
+end
+
+namespace :deploy do
+  desc 'Restart the Application'
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  namespace :shared do
+    desc 'Symlink shared content and configuration'
+    task :symlinks do
+      create_symlink 'config/database.yml'
+      create_symlink 'config/secret.txt'
+    end
+  end
+end
+
+after 'deploy:finalize_update', 'deploy:shared:symlinks'

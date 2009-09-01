973166452c50996786cabcfc70ca9d848ff06d9c
Xavier Shay <xavier@rhnh.net>
2009-09-01 16:17:01 +0300

    Patch backtrace cleaner to clean gem paths properly - this has been submitted as a rails patch

---
+++ config/initializers/backtrace_cleaner.rb
@@ -0,0 +1,16 @@
+module Rails
+  class BacktraceCleaner < ActiveSupport::BacktraceCleaner
+    private
+      GEM_REGEX = "([A-Za-z_-]+)-([0-9.]+)"
+
+      def add_gem_filters
+        Gem.path.each do |path|
+          # http://gist.github.com/30430
+          add_filter { |line| line.sub(/(#{path})\/gems\/#{GEM_REGEX}\/(.*)/, '\2 (\3) \4')}
+        end
+
+        vendor_gems_path = Rails::GemDependency.unpacked_path.sub("#{RAILS_ROOT}/",'')
+        add_filter { |line| line.sub(/(#{vendor_gems_path})\/#{GEM_REGEX}\/(.*)/, '\2 (\3) [v] \4')}
+      end
+  end
+end

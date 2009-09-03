7dcefbc7afa588e27b30f515109cafe0d19641c1
Xavier Shay <xavier@rhnh.net>
2009-09-03 12:24:29 +0300

    Show shipping status on the order show page

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class OrdersControllerTest < ActionController::TestCase
   should_route :get,    '/us/orders/new',   :action => 'new',     :distributor_id => 'us'

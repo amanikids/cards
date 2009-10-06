862c5b16996d42408652b53fd6bab3093038fd2c
Matthew Todd <matthew.todd@gmail.com>
2009-10-06 08:58:02 +0300

    Quick patch.

---
+++ db/migrate/20091006052134_fix_dinas_zipcode.rb
@@ -0,0 +1,12 @@
+class FixDinasZipcode < ActiveRecord::Migration
+  def self.up
+    Distributor.find_by_name('Dina Sciarra').tap do |dina|
+      dina.donation_methods.find_by_name('check').tap do |check|
+        check.update_attributes(:description => "Make your check payable to \"Friends of Amani US\" and send it to:\n\nDina Sciarra\n32 Teak Loop\nOscala, FL\n34472")
+      end
+    end
+  end
+
+  def self.down
+  end
+end

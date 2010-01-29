d4a7e09a51656b37566d03eb5faa8f2cf5669c65
Matthew Todd <matthew.todd@gmail.com>
2010-01-29 10:43:35 +0300

    Remove cruft comments from environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,9 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Skip frameworks you're not going to use. To use Rails without a database
-  # you must remove the Active Record framework.
   config.frameworks -= [:active_resource]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Make Time.zone default to the specified zone, and make Active Record store time values
-  # in the database in UTC, and return them converted to the specified local zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
-
-  # Activate observers that should always be running
   config.active_record.observers = :inventory_cache_observer, :notification_observer
 end

3348a4a3dbe04c3d3bf499d5eb95ac5436490078
Matthew Todd <matthew.todd@gmail.com>
2010-01-29 10:42:58 +0300

    Externalize session configuration.

--- config/environment.rb
+++ config/environment.rb
@@ -19,15 +19,6 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
 
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random,
-  # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session = {
-    :session_key => '_cards_amanikids_org_session',
-    :secret      => '4ebe2454ead4605b7cb93f8a5fe67a613f5350197a8cc6f4bda8c09d4ccf99d2fc0e012b2f2b0ff5e2ff97bf2fa0b9e441bc54bdc3d4d8a14dd29a468eb340e0'
-  }
-
   # Activate observers that should always be running
   config.active_record.observers = :inventory_cache_observer, :notification_observer
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+ActionController::Base.session = {
+  :key    => '_cards_amanikids_org_session',
+  :secret => '4ebe2454ead4605b7cb93f8a5fe67a613f5350197a8cc6f4bda8c09d4ccf99d2fc0e012b2f2b0ff5e2ff97bf2fa0b9e441bc54bdc3d4d8a14dd29a468eb340e0'
+}

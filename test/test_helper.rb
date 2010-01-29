1f2199e5d57dc77dadf57205443bd8b81cc229ab
Matthew Todd <matthew.todd@gmail.com>
2010-01-29 10:37:03 +0300

    Should doesn't seem to be rspec/testunit confused.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,6 @@ require 'test_help'
 require File.expand_path(File.dirname(__FILE__) + '/factories')
 require 'mocha'
 
-# Running from rake, if we just require 'shoulda', shoulda sees the Spec
-# constant defined by Cucumber (loaded in lib/tasks/testing.rake) and thinks
-# it should run in RSpec mode instead of Test::Unit mode. Bad shoulda.
-require 'shoulda/test_unit'
-require 'shoulda/rails'
-
 if $stdin.tty?
   require 'redgreen'
 end

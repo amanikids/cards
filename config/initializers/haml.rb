33b2341eaa55d1b489e6cb1025c5fb2f24a6ab03
Matthew Todd <matthew.todd@gmail.com>
2010-09-22 09:52:09 +0300

    HTML5. Kind of.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,5 +1,5 @@
-# It seems like Haml's init.rb isn't being called when we have it vendored as a gem.
-Haml.init_rails(binding)
+# Rock the 5
+Haml::Template.options[:format] = :html5
 
 # Tell Haml to wrap html attributes with double instead of single quotes.
 Haml::Template.options[:attr_wrapper] = '"'

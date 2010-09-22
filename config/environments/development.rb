fc84e05748e6f554ab026bbd488bc27c1e2ae34c
Matthew Todd <matthew.todd@gmail.com>
2010-09-22 09:16:24 +0300

    Don't send developer mail in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,4 @@ config.action_controller.perform_caching             = false
 
 # ActionMailer settings
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method     = :sendmail
-config.action_mailer.sendmail_settings   = { :location => '/usr/sbin/sendmail', :arguments => "-i #{ENV['USER']}" }
 config.action_mailer.default_url_options = { :host => 'shoes.local:3000' }

add1171fba978b8f76831a5b54603c853018fc99
Matthew Todd <matthew.todd@gmail.com>
2010-10-13 16:07:04 +0300

    Styling the sign-in form.

---
+++ config/initializers/cacheable_assets.rb
@@ -0,0 +1,9 @@
+# Generate stylesheets into tmp.
+Sass::Plugin.options[:template_location] = Rails.root.join('app', 'stylesheets').to_s
+Sass::Plugin.options[:css_location]      = Rails.root.join('tmp', 'stylesheets').to_s
+
+# Serve most assets from public, stylesheets from tmp.
+CacheableAssets.configure do |static_asset_paths|
+  static_asset_paths['public'] = %w( /favicon.ico /images /javascripts )
+  static_asset_paths['tmp']    = %w( /stylesheets )
+end

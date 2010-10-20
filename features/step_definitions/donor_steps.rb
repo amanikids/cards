8c400a599064dc959babe8b2a101063cabf8960a
Matthew Todd <matthew.todd@gmail.com>
2010-10-20 15:42:58 +0300

    More PayPal scaffolding.

--- features/step_definitions/donor_steps.rb
+++ features/step_definitions/donor_steps.rb
@@ -5,8 +5,14 @@ Given /^these products are for sale:$/ do |table|
 end
 
 Given /^I will be using PayPal Express Checkout$/ do
-  ShamRack.mount(ShamPayPalAPI.new, 'api-3t.sandbox.paypal.com', 443)
-  ShamRack.mount(ShamPayPalUI.new,  'www.sandbox.paypal.com', 443)
+  paypal = ShamPayPal.new
+
+  ShamRack.mount(paypal, 'api-3t.sandbox.paypal.com', 443)
+
+  Capybara.app = Rack::URLMap.new(
+    'https://www.sandbox.paypal.com/' => paypal,
+    'http://www.example.com/'         => Rails.application
+  )
 end
 
 Then /^I see the following order:$/ do |expected|
--- features/support/paypal.rb
+++ features/support/paypal.rb
@@ -1,36 +1,75 @@
-class ShamPayPalAPI < Sinatra::Base
+class ShamPayPal < Sinatra::Base
+  get '/cgi-bin/webscr' do
+    send params[:cmd].underscore, params
+  end
+
   post '/2.0/' do
-    handle Nokogiri::XML(request.body)
+    xml = Nokogiri::XML(request.body)
+    xml = xml.xpath('/env:Envelope/env:Body/*/*').first
+    send xml.name.underscore, xml
   end
 
   not_found do
-    abort "Unhandled request: #{request.request_method} #{request.path}"
+    abort "404: #{request.request_method} #{request.path}"
   end
 
-  def handle(xml)
-    dispatch xml.xpath('/env:Envelope/env:Body/*/*').first
+  error do
+    abort env['sinatra.error'].message
   end
 
-  def dispatch(xml)
-    if respond_to?(method_name = xml.name.underscore)
-      send method_name, xml
-    else
-      abort "Method #{method_name} is not defined."
-    end
+  # Web UI Methods ----------------------------------------------------
+  def _express_checkout(params)
+    redirect @@xml_request['ReturnURL']
   end
 
+  # API Methods -------------------------------------------------------
   def set_express_checkout_request(xml)
+    capture_xml_request(xml)
+
     return <<-XML
       <SetExpressCheckoutResponse>
-        <ACK>Success</ACK>
-        <TOKEN>42</TOKEN>
+        <Ack>Success</Ack>
+        <Token>42</Token>
       </SetExpressCheckoutResponse>
     XML
   end
-end
 
-class ShamPayPalUI < Sinatra::Base
-  not_found do
-    abort "Unhandled request: #{request.request_method} #{request.path}"
+  def get_express_checkout_details_request(xml)
+    return <<-XML
+      <GetExpressCheckoutDetailsResponse>
+        <Ack>Success</Ack>
+        <GetExpressCheckoutDetailsResponseDetails>
+          <Token>42</Token>
+          <PayerInfo>
+            <Payer>bob@example.com</Payer>
+            <PayerID>1234567890</PayerID>
+          </PayerInfo>
+          <PaymentDetails>
+            <ShipToAddress>
+              <Name>Bob Loblaw</Name>
+              <Street1>123 Main St.</Street1>
+              <Street2></Street2>
+              <CityName>Anytown</CityName>
+              <StateOrProvince>AB</StateOrProvince>
+              <Country>US</Country>
+              <PostalCode>12345</PostalCode>
+            </ShipToAddress>
+          </PaymentDetails>
+        </GetExpressCheckoutDetailsResponseDetails>
+      </GetExpressCheckoutDetailsResponse>
+    XML
+  end
+
+  private
+
+  def capture_xml_request(xml)
+    @@xml_request ||= {}
+    xml.elements.each do |element|
+      if element.elements.any?
+        capture_xml_request(element)
+      else
+        @@xml_request[element.name] = element.text
+      end
+    end
   end
 end

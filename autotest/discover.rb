0e4e499c75594c9c1926ed988207db137c28c614
Matthew Todd <matthew.todd@gmail.com>
2010-10-25 15:24:35 +0300

    Don't bundle exec rspec in autotest.
    Gets to running tests much faster this way!

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,13 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+
+Autotest.add_hook(:initialize) do
+  class ::Autotest::RailsRspec2
+    # Run rspec without using bundle exec.
+    # - Unnecessary with rpsec installed in an rvm gemset.
+    # - 3-4 seconds faster startup time!
+    def using_bundler?
+      false
+    end
+  end
+end

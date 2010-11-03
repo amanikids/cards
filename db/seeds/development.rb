bd0a7f0521f278cd9c95391d89924014b31af42b
Matthew Todd <matthew.todd@gmail.com>
2010-11-03 14:27:58 +0300

    Seed 2 cards.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -30,6 +30,27 @@ Store.find_or_initialize_by_slug('uk').tap do |store|
       )
     end
   end
+
+  store.products.find_or_initialize_by_name('Pinebough').tap do |card|
+    card.update_attributes!(
+      :description => 'Lovely, just lovely!',
+      :image_path  => '2010/pinebough.jpg'
+    )
+
+    card.packagings.find_or_initialize_by_size(10) do |packaging|
+      packaging.update_attributes!(
+        :name  => '10-pack',
+        :price => 1000
+      )
+    end
+
+    card.packagings.find_or_initialize_by_size(25) do |packaging|
+      packaging.update_attributes!(
+        :name  => '25-pack',
+        :price => 2500
+      )
+    end
+  end
 end
 
 Store.find_or_initialize_by_slug('us').tap do |store|
@@ -66,6 +87,26 @@ Store.find_or_initialize_by_slug('us').tap do |store|
     end
   end
 
+  store.products.find_or_initialize_by_name('Pinebough').tap do |card|
+    card.update_attributes!(
+      :description => 'Lovely, just lovely!',
+      :image_path  => '2010/pinebough.jpg'
+    )
+
+    card.packagings.find_or_initialize_by_size(10) do |packaging|
+      packaging.update_attributes!(
+        :name  => '10-pack',
+        :price => 1000
+      )
+    end
+
+    card.packagings.find_or_initialize_by_size(25) do |packaging|
+      packaging.update_attributes!(
+        :name  => '25-pack',
+        :price => 2500
+      )
+    end
+  end
   store.carts.create!.tap do |cart|
     cart.items.create!(
       :packaging_id => store.products.first.packagings.first.id,
